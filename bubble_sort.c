c11da41bc07571f77a502077e89111df314ae7dc
Dhrub Shah <78858602+DhrubShah@users.noreply.github.com>
2024-07-27 18:30:59 +0545

    bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to perform bubble sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Given array is \n");
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    printf("\nSorted array is \n");
+    printArray(arr, n);
+    
+    return 0;
+}
